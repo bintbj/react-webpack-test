ede571636b97e0a5067e9c3983fe8cf7087d004e
bintbj <bintbj@gmail.com>
2016-11-25 11:58:36 +0800

    learn-react

---
+++ app/Hobby.jsx
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+
+const propTypes = {
+  hobby: PropTypes.string.isRequired
+};
+
+function Hobby(props) {
+  return <li>{props.hobby}</li>;
+}
+Hobby.propTypes = propTypes;
+
+export default Hobby;
---
+++ app/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Profile from './Profile';
+const ele = document.createElement('div');
+document.body.appendChild(ele);
+const props = {
+  name: 'viking',
+  age: 20
+};
+render(<Profile {...props} />, ele);
